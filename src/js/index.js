fd0d3b04a7006df9f9a2de5604f3d05411b31ccd
m-ikiara <brnikiara@gmail.com>
2024-03-26 14:52:54 +0300

    fix: totally scrap `asyncio`

--- src/js/index.js
+++ src/js/index.js
@@ -5,23 +5,23 @@ connection.onopen = () => {
 	const api = new DerivAPIBasic({ connection });
 	const tick_stream = () => api.subscribe({ ticks: 'R_100' });
 
-	const tick_response = async (response) => {
+	const tick_response = (response) => {
 		const data = JSON.parse(response.data);
 		if (data.error !== undefined) {
 			console.error(`[ERROR] ${data.error.message}`);
 			connection.removeEventListener('message', tickResponse, false);
 			api.disconnect();
 		}
-		await if (data.msg_type === 'tick') console.log(data.tick);
+		if (data.msg_type === 'tick') console.log(data.tick);
 	};
 
-	const subscribe_ticks = async () => {
-		await tick_stream();
+	const subscribe_ticks = () => {
+		tick_stream();
 		connection.addEventListener('message', tick_response);
 	}
 
-	const unsubscribe_ticks = async () => {
-		await connection.removeEventListener('message', tick_response, false);
+	const unsubscribe_ticks = () => {
+		connection.removeEventListener('message', tick_response, false);
 		tick_stream().unsubscribe();
 	}
 
